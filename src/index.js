90e08aa593917feb974517c3e891f444aa21a07a
briecodes <hello@brie.work>
2018-06-08 00:24:47 -0400

    lab completed.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+clickHandler = () => {
+  console.log("onClick Event")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={clickHandler} />
   </div>,
   document.getElementById('root')
 );
