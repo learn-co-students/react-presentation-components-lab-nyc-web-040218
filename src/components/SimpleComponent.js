78ea621e182d7537a1194f38bb1bfca8d58e2a81
Matthew Talbot <mdtalbot@gmail.com>
2018-06-29 10:59:52 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,35 @@
 // Code SimpleComponentHere Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default class SimpleComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mood: 'happy'
+    };
+  }
+
+  handleClick = (event) => {
+    if (this.state.mood === 'happy') {
+     return this.setState({mood: 'sad'})
+    }
+    if (this.state.mood === 'sad')
+     return this.setState({mood: 'happy'})
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        {this.state.mood}
+      </div>
+    );
+  }
+}
+
+ SimpleComponent.propTypes = propTypes;
+ SimpleComponent.defaultProps = defaultProps;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={alert('EVERYBODY DANCE NOW')} />
   </div>,
   document.getElementById('root')
 );
